1edf6604efffce092761d81e11c7633548e14b69
fatihduzenli <fatihdznl1991@gmail.com>
2024-09-10 19:00:49 -0400

    Resilience4j implemented

--- src/main/java/com/cydeo/client/ProjectClient.java
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -2,6 +2,8 @@ package com.cydeo.client;
 
 import com.cydeo.dto.ProjectResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ public interface ProjectClient {
 
     @GetMapping("/api/v1/project/count/manager/{assignedManager}")
     @CircuitBreaker(name = "project-service")
+    @Retry(name = "project service")
+    @RateLimiter(name = "project-service")
     ResponseEntity<ProjectResponse> getNonCompletedCountByAssignedManager(@PathVariable("assignedManager") String assignedManager);
 
 }
--- src/main/java/com/cydeo/client/TaskClient.java
+++ src/main/java/com/cydeo/client/TaskClient.java
@@ -2,6 +2,8 @@ package com.cydeo.client;
 
 import com.cydeo.dto.TaskResponse;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +14,7 @@ public interface TaskClient {
 
     @GetMapping("/api/v1/task/count/employee/{assignedEmployee}")
     @CircuitBreaker(name = "task-service")
+    @Retry(name = "task-service")
+    @RateLimiter(name = "task-service")
     ResponseEntity<TaskResponse> getNonCompletedCountByAssignedEmployee(@PathVariable("assignedEmployee") String assignedEmployee);
 }
